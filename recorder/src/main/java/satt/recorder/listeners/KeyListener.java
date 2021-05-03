2ab887e6a428e79c97e05e85497f31c9bcda2d78
Мириан Джачвадзе <mjachvadze@alfabank.ru>
2021-05-03 15:31:19 +0300

    pollution

--- recorder/src/main/java/satt/recorder/listeners/KeyListener.java
+++ recorder/src/main/java/satt/recorder/listeners/KeyListener.java
@@ -65,44 +65,6 @@ public class KeyListener extends SwingKeyAdapter {
                 break;
             }
         }
-
-
-//        if (e.getModifiers() & NativeKeyEvent.META_MASK
-//                 && e.getModifiers() & NativeKeyEvent.CTRL_MASK) {
-//            System.out.print("Attempting to consume CMD+F9 event...\t");
-//        }
-
-        // Cmd + F9
-//        if ((e.getModifiers() & NativeKeyEvent.META_MASK) != 0
-//                && e.getKeyCode() == NativeKeyEvent.VC_F9) {
-//            System.out.print("Attempting to consume CTRL+ALT+DEL event...\t");
-//            try {
-//                System.out.print("[ OK ]\n");
-//            }
-//            catch (Exception ex) {
-//                System.out.print("[ !! ]\n");
-//                ex.printStackTrace();
-//            }
-//        }
-
-//        Ctrl + Alt + Delete
-//        if ((e.getModifiers() & NativeKeyEvent.CTRL_MASK) != 0
-//                && (e.getModifiers() & NativeKeyEvent.ALT_MASK) != 0
-//                && e.getKeyCode() == NativeKeyEvent.VC_DELETE) {
-//            System.out.print("Attempting to consume CTRL+ALT+DEL event...\t");
-//            try {
-//                Field f = NativeInputEvent.class.getDeclaredField("reserved");
-//                f.setAccessible(true);
-//                f.setShort(e, (short) 0x01);
-//
-//                System.out.print("[ OK ]\n");
-//            }
-//            catch (Exception ex) {
-//                System.out.print("[ !! ]\n");
-//                ex.printStackTrace();
-//            }
-//        }
-//
     }
 
     @Override
